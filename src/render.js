5f0d67342e2478f9f27ec8844fc236db119844d1
ankitshah86 <ankit.r.shah63@gmail.com>
2021-01-28 08:55:12 -0500

    basic mouse move drawing works

--- src/render.js
+++ src/render.js
@@ -1,6 +1,10 @@
 var drawingboard = document.getElementById("drawingboard")
-var contxt = drawingboard.getContext("2d")
+var ctx = drawingboard.getContext("2d")
 
 drawingboard.addEventListener(('mousemove'),function(ev) {
+    var ctx = drawingboard.getContext("2d")
     console.log({x : ev.x,y : ev.y})
+    ctx.fillStyle = "orange"
+    ctx.fillStyle = 'rgb(255, 165, 35)';
+    ctx.fillRect(ev.clientX - drawingboard.offsetLeft, ev.clientY - drawingboard.offsetTop, 5, 5)
 })
