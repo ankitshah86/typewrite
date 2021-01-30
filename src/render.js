0f13ab568c9fa2578d3a1ab1e6215a30ae963c9c
ankitshah86 <ankit.r.shah63@gmail.com>
2021-01-29 21:08:19 -0500

    hand drawing now works

--- src/render.js
+++ src/render.js
@@ -1,10 +1,40 @@
 var drawingboard = document.getElementById("drawingboard")
+drawingboard.width = 300
+drawingboard.height = 300
 var ctx = drawingboard.getContext("2d")
+var pathBegin
+var prevX, prevY
 
-drawingboard.addEventListener(('mousemove'),function(ev) {
-    var ctx = drawingboard.getContext("2d")
-    console.log({x : ev.x,y : ev.y})
+/**
+ * 
+ * @param {CanvasRenderingContext2D} ctx 
+ */
+var init = function (ctx) {
+
     ctx.fillStyle = "orange"
     ctx.fillStyle = 'rgb(255, 165, 35)';
-    ctx.fillRect(ev.clientX - drawingboard.offsetLeft, ev.clientY - drawingboard.offsetTop, 5, 5)
+    ctx.strokeStyle = "blue"
+    ctx.lineWidth = 3;
+    pathBegin = false
+}
+
+init(ctx)
+
+drawingboard.addEventListener(('mousemove'), function (ev) {
+    let x = ev.pageX - drawingboard.offsetLeft
+    let y = ev.pageY - drawingboard.offsetTop
+
+    if (ev.ctrlKey || ev.buttons  == 1) {
+
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.closePath();
+    }
+    prevX = x
+    prevY = y
+
+
+    //console.log({ x: ev.x, y: ev.y })
 })
